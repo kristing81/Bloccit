70df065f8684f075f55532481ab95bf4c7f8cd7f
Kristin Gonzalez <kristin_gonzalez@yahoo.com>
2014-07-22 11:44:21 +0800

    First commit and README update

---
+++ README.md
@@ -0,0 +1,26 @@
+== README
+## Bloccit: a Reddit clone to teach the fundamentals of web development and Rails 4.
+
+Made with my mentor at [Bloc](http://bloc.io).
+
+* Ruby version 2.1.2
+
+* System dependencies
+
+* Configuration
+
+* Database creation
+
+* Database initialization
+
+* How to run the test suite
+
+* Services (job queues, cache servers, search engines, etc.)
+
+* Deployment instructions
+
+* ...
+
+
+Please feel free to use a different markup language if you do not plan to run
+<tt>rake doc:app</tt>.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloccit::Application.config.secret_key_base = '2c3540689419580257c290ae2307f3954ccd72eadeed33cc26e28e44ee9b686985562f27fec0f26e459b185e6d2f1a3f82b12d0f9d16b140ab05a4e57589d6ea'
