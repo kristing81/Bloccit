fc022d9fe1eacb559d126913d68d9598bee57039
Kristin Gonzalez <kristin_gonzalez@yahoo.com>
2014-08-13 17:58:45 +0800

    Private topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,8 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+
+  scope :private, -> { where(public: false) }
+  scope :public, -> { where(public: true) }
+  scope :visible_to, -> (user) { user ? all : where(public: true) }
+
 end
