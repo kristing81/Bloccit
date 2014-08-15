2bfd5162dcc830705b7da9c65fb0ba98c2f476b3
Kristin Gonzalez <kristin_gonzalez@yahoo.com>
2014-08-15 16:59:44 +0800

    Popular posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Bloccit::Application.routes.draw do
 
   devise_for :users
-    resources :users, only: [:update, :show]
-
-   resources :topics do
-     resources :posts, except: [:index] do
-      # resources :comments, only: [:create, :destroy]
-      get '/up-vote' => 'votes#up_vote', as: :up_vote
-      get '/down-vote' => 'votes#down_vote', as: :down_vote
-    end
+  resources :users, only: [:update, :show, :index]
+  resources :topics do
+    resources :posts, except: [:index], controller: 'topics/posts'
   end
 
-  resources :posts, only: [] do
+  resources :posts, only: [:index] do
     resources :comments, only: [:create, :destroy]
     resources :favorites, only: [:create, :destroy]
+    get '/up-vote' => 'votes#up_vote', as: :up_vote
+    get '/down-vote' => 'votes#down_vote', as: :down_vote
   end
 
   get 'about' => 'welcome#about'
