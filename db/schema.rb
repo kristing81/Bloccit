82ede3bef04367fc56005e214756afbdb0d6e054
Kristin Gonzalez <kristin_gonzalez@yahoo.com>
2014-08-19 19:29:05 +0800

    Fixing schema on heroku

--- db/schema.rb
+++ db/schema.rb
@@ -18,11 +18,9 @@ ActiveRecord::Schema.define(version: 20140812033626) do
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "favorites", force: true do |t|
     t.integer  "post_id"
@@ -81,7 +79,7 @@ ActiveRecord::Schema.define(version: 20140812033626) do
 
   create_table "votes", force: true do |t|
     t.integer  "value"
-    t.string   "user_id"
+    t.integer  "user_id"
     t.string   "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
