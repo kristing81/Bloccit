4cb9373b429c1c448cde41c1e5fb6016c8bee060
Kristin Gonzalez <kristin_gonzalez@yahoo.com>
2014-08-19 19:11:10 +0800

    Ensure vote user_id is an integer

--- db/migrate/20140810043220_create_votes.rb
+++ db/migrate/20140810043220_create_votes.rb
@@ -2,7 +2,7 @@ class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
       t.integer :value
-      t.string :user_id
+      t.integer :user_id
       t.string :post_id
 
       t.timestamps
