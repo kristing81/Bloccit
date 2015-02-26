9fada451ef3155f8156787574a36400373a22dbf
Kristin Gonzalez <kristin_gonzalez@yahoo.com>
2015-02-26 14:52:48 +0800

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -77,10 +77,6 @@ ActiveRecord::Schema.define(version: 20140812033626) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
-  create_table "votes", force: true do |t|
-    t.integer  "value"
-    t.integer  "user_id"
-    t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
